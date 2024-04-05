3c96c013af18ca626af1a8abe380cdd5f818cbe8
Ida Casperson <ida_casperson@hotmail.com>
2024-04-05 09:12:26 +0200

    change comments in eslint file

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,23 +10,23 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   plugins: ["react-refresh"],
   rules: {
-    // Regelbrott: Användning av console.log.
+    // Användning av console.log.
     // Förbättring: Undvik användning av console.log i produktionskoden.
     "no-console": "warning",
 
-    // Regelbrott: Använd icke-strikta jämförelseoperatorer
+    // Använd icke-strikta jämförelseoperatorer
     // Förbättring: Använd alltid strikt jämförelseoperator (=== eller !==) för att undvika oväntade beteenden.
     eqeqeq: "warn",
 
-    // Regelbrott: Användning av icke-använda variabler.
+    // Användning av icke-använda variabler.
     // Förbättring: Ta bort eller använd variabler som deklarerats men inte används.
     "no-unused-vars": "error",
 
-    // Regelbrott: använda alert, confirm, och prompt.
+    // använda alert, confirm, och prompt.
     // Förbättring: Undvik användning av dessa metoder för att förhindra att användaren distraheras eller irriteras.
     "no-alert": "warn",
 
-    // Regelbrott: Användning av dubbla citattecken i stället för enkla citattecken.
+    // Användning av dubbla citattecken i stället för enkla citattecken.
     // Förbättring: Standardisera användning av citattecken för att öka konsistens.
     quotes: ["error", "double"],
   },
